d32dbe8dc379eb5d6221b59ac2de2125818ca241
wandell <wandell@stanford.edu>
2018-10-16 17:22:00 -0700

    More comments

--- sensorCompute/c_sensorCompute.m
+++ sensorCompute/c_sensorCompute.m
@@ -1,36 +1,31 @@
 function rgb = c_sensorCompute()
-% Create code to compile for producing a sensor output data file
+% Create compiled code for producing sensor RGB data
 %
-% c_sensorCompute
+% This function is not interesting in itself.
 %
-% We successfully compiled once using restoredefaultpath and then running.
-% The output is a run_c_sensorCompute.sh file.
-% That one executes by 
-%      run_c_sensorCompute.sh $MCROOT, where on the GCP $MCROOT is
-%     ./run_c_sensorCompute.sh /software/MATLAB/R2017b
+% The comments on how to compile this code are in the _build file.
 %
-% But in that case it complained it couldn't find the sceneCreate function.
-% Why is that?
+% ZL/BW
 %
-% When we run with everything on the path and no restoredefaultpath, the
-% compiler generates a warning about finding things.  But it does seem to
-% find stuff.  What fails on is finding data files, like luminosity.mat.
-%
-% The next problem, then, is that the compiled code doesn't know where the
-% data/human/luminosity.mat file is.
-% 
-% EXAMPLE COMPILATION:
-%     git clone https://github.com/ISET/isetcam
-%
-%     In Matlab (e.g., r2015b):
-%       mcc -m isetcam/compile/c_sensorCompute.m -I isetcam
+% See also
+%   c_sensorCompute_build
 %
 
-scene = sceneCreate;
+%{
+ system('./run_c_sensorCompute.sh /software/MATLAB/R2017b');
+ load('sensor1','rgb');
+ vcNewGraphWin; imagesc(rgb);
+%}
+
+scene = sceneCreate('mackay');
+scene = sceneSet(scene,'fov',5);
 oi = oiCreate;
 oi = oiCompute(oi,scene);
+
 sensor = sensorCreate;
+sensor = sensorSetSizeToFOV(sensor,4);
 sensor = sensorCompute(sensor,oi);
+
 rgb = sensorGet(sensor,'rgb');
 save('sensor1','rgb');
 
