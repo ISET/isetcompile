f371d1f9403cc02fccce24a667a73d9a4482573a
wandell <wandell@stanford.edu>
2018-10-18 11:49:39 -0700

    renamed.  Decided to keep only the files that build the compiled function, not the compiled function itself (which is large) or the run shell script

--- sensorCompute/c_sensorCompute.m
+++ sensorCompute/c_sensorCompute.m
@@ -1,4 +1,4 @@
-function rgb = c_sensorCompute()
+function rgb = c_sensorCompute(varargin)
 % Create compiled code for producing sensor RGB data
 %
 % This function is not interesting in itself.
@@ -12,22 +12,65 @@ function rgb = c_sensorCompute()
 %
 
 %{
- system('./run_c_sensorCompute.sh /software/MATLAB/R2017b');
- load('sensor1','rgb');
- vcNewGraphWin; imagesc(rgb);
+% Check the function running locally.
+
+% Create the parameters
+params = c_sensorComputeParams;
+params.sceneFov  = 3;
+params.sensorFov = 4;
+params.outfile   = 'localOut';
+
+
+jsonwrite('c_sensorCompute.json',params);
+c_sensorCompute('jsonFile','c_sensorCompute.json');
+load(params.outfile,'rgb');
+vcNewGraphWin; imagesc(rgb);
+%}
+%{
+% Write this function
+params = c_sensorComputeParams;
+
+% Set the parameters
+params.sceneType = 'frequency orientation';
+params.sceneFov  = 4;
+params.sensorFov = 6;
+params.outfile   = 'localOut';
+
+% Must be within the directory because we need to know the path
+chdir(fullfile(isetCompileRootPath,'sensorCompute'));
+jsonwrite('c_sensorCompute.json',params);
+
+% Execute the code
+system('./run_c_sensorCompute.sh /software/MATLAB/R2017b');
+load(params.outfile,'rgb');
+vcNewGraphWin; imagesc(rgb);
 %}
 
-scene = sceneCreate('mackay');
-scene = sceneSet(scene,'fov',5);
+%%  When compiled, the inputs always appear to be strings
+
+p = inputParser;
+
+p.addParameter('jsonfile','c_sensorCompute.json');
+p.parse(varargin{:});
+
+jsonFile = p.Results.jsonfile;
+params = jsonread(jsonFile);
+
+%%  Create the scene
+scene = sceneCreate(params.sceneType);
+scene = sceneSet(scene,'fov',params.sceneFov);
+
+%%
 oi = oiCreate;
 oi = oiCompute(oi,scene);
 
+%%
 sensor = sensorCreate;
-sensor = sensorSetSizeToFOV(sensor,4);
+sensor = sensorSetSizeToFOV(sensor,params.sensorFov);
 sensor = sensorCompute(sensor,oi);
 
 rgb = sensorGet(sensor,'rgb');
-save('sensor1','rgb');
+save(params.outfile,'rgb');
 
 end
 
---
+++ sensorCompute/c_sensorComputeParams.m
@@ -0,0 +1,16 @@
+function params = c_sensorComputeParams
+% Initialize the parameters for the c_sensorCompute function
+%
+%
+% ZL/BW 
+%
+% See also
+%  c_sensorCompute
+%
+
+params.sceneType = 'macbeth';
+params.sceneFov = 5;
+params.sensorFov = 4;
+params.outfile = 'sensorCompute';
+
+end
